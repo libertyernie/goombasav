e15973cf115b5f72376f1db26ab312657ba50595
libertyernie <maybeway36@gmail.com>
2014-05-04 17:54:00 -0500

    Add words to about form

--- goombasav_cs/AboutForm.Designer.cs
+++ goombasav_cs/AboutForm.Designer.cs
@@ -23,11 +23,128 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(240, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Goomba Save Manager";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(14, 34);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(82, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "2014-05-04";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.Location = new System.Drawing.Point(12, 88);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(433, 228);
+			this.label3.TabIndex = 2;
+			this.label3.Text = resources.GetString("label3.Text");
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.AutoSize = true;
+			this.button1.Location = new System.Drawing.Point(12, 319);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(79, 27);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "View GPL";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(14, 51);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(137, 17);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "(C) 2014 libertyernie";
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.Location = new System.Drawing.Point(330, 41);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(115, 27);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "View on GitHub";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label5
+			// 
+			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label5.Location = new System.Drawing.Point(12, 77);
+			this.label5.Margin = new System.Windows.Forms.Padding(3, 9, 3, 9);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(433, 2);
+			this.label5.TabIndex = 6;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(290, 9);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(155, 17);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "C# version / C++ library";
+			// 
+			// AboutForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "AboutForm";
+			this.ClientSize = new System.Drawing.Size(457, 358);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Name = "AboutForm";
+			this.Text = "About";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
 	}
 }
--- goombasav_cs/AboutForm.cs
+++ goombasav_cs/AboutForm.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace goombasav_cs {
 	public partial class AboutForm : Form {
 		public AboutForm() {
 			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			Process.Start("http://www.gnu.org/licenses/gpl-3.0.html");
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			Process.Start("https://github.com/libertyernie/goombasav");
 		}
 	}
 }
