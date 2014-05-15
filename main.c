7a4d285c6cfc07afffa73b79005e6b7fd0e1da6d
libertyernie <maybeway36@gmail.com>
2014-05-15 14:15:27 -0500

    Add license notices to files

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
-// goombasav command line version.
-// Works with gcc, g++, and Visual Studio 2013 (for the latter, use /TP to compile as C++ code)
+/* main.c - command line interface to goombasav
+
+Copyright (C) 2014 libertyernie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+https://github.com/libertyernie/goombasav
+
+When compiling in Visual Studio, set all goombasav files to compile
+as C++ code (Properties -> C/C++ -> Advanced -> Compile As.)
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
