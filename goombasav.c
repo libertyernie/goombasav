c0645d5faf616b5f172068407202506346a99ead
libertyernie <maybeway36@gmail.com>
2014-09-08 16:15:54 -0500

    fix for ppc arch

--- goombasav.c
+++ goombasav.c
@@ -189,7 +189,7 @@ stateheader* stateheader_for(const void* gba_data, const char* gbc_title) {
 	stateheader** headers = stateheader_scan(gba_data);
 	int i;
 	for (i = 0; headers[i] != NULL; i++) {
-		if (strcmp(headers[i]->title, title) == 0 && headers[i]->type == GOOMBA_SRAMSAVE) {
+		if (strcmp(headers[i]->title, title) == 0 && F16(headers[i]->type) == GOOMBA_SRAMSAVE) {
 			use_this = headers[i];
 			break;
 		}
